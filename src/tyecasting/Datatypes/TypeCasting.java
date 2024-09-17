5ed4cd334127389b96e31f67fb414f001295a4fe
dell <dell@Suraj--PC>
2024-09-17 11:12:13 +0530

    JavaBasics

---
+++ src/tyecasting/Datatypes/TypeCasting.java
@@ -0,0 +1,14 @@
+package tyecasting.Datatypes;
+
+public class TypeCasting {
+	public static void main (String [] a) {
+//		double doub=10.2;
+//		int intval=(int) doub;
+//		System.out.println(intval);//Explicit TypeCating
+		
+//		boolean p=true;
+//		int x=(int)p;
+//		System.out.println(x);//Since it can't perform
+		
+	}
+}
---
+++ src/tyecasting/Datatypes/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package tyecasting.Datatypes;
+
